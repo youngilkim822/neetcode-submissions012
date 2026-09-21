4ce971aeffc890d1d4dd40e17142552bef486864
Youngil Kim <youngilkim822@gmail.com>
2026-09-20 20:59:09 -0400

    Add: evaluate-reverse-polish-notation - submission-2

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-2.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        if(tokens == null || tokens.length == 0) return 0;
+
+        Stack<String> stack = new Stack<>();
+        for(String token : tokens){
+            if(token.equals("+") || token.equals("*") || token.equals("-") || token.equals("/")){
+                helper(stack, token);
+            }else{
+                stack.push(token);
+            }
+        }
+        if(stack.isEmpty()) return 0;
+        return Integer.valueOf(stack.pop());
+    }
+
+    private void helper(Stack<String> stack, String operator){
+        Integer temp1 = 0;
+        Integer temp2 = 0;
+        if(!stack.isEmpty()){
+            temp1 = Integer.valueOf(stack.pop());
+        }
+        if(!stack.isEmpty()){
+            temp2 = Integer.valueOf(stack.pop());
+        }   
+        int ans = 0;
+        if(operator.equals("+")){
+            ans = temp1 + temp2;
+        }else if(operator.equals("*")){
+            ans = temp1 * temp2;
+        }else if(operator.equals("-")){
+            ans = temp2 - temp1;
+        }else if(operator.equals("/")){
+            ans = temp2 / temp1;
+        }
+        stack.push(Integer.toString(ans));
+    }
+}

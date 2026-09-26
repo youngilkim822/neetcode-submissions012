1f72a2bf7cd9f9b31d948e27b9db27c9cb3398f9
Youngil Kim <youngilkim822@gmail.com>
2026-09-25 22:23:38 -0400

    Add: combination-target-sum - submission-2

---
+++ Data Structures & Algorithms/combination-target-sum/submission-2.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+        if(nums == null) return new ArrayList<>();
+
+        List<List<Integer>> list = new ArrayList<>();
+        recurse(nums, list, new ArrayList<>(), target, 0, 0);
+        return list;
+    }
+
+    private void recurse(int[] nums, List<List<Integer>> list, List<Integer> innerList, int target, int index, int sum){
+        if(sum == target){
+            list.add(new ArrayList<>(innerList));
+            return;
+        }else if(sum > target){
+            return;
+        }
+
+        if(index == nums.length){
+            return;
+        }
+
+        for(int i=index; i<nums.length; i++){
+            innerList.add(nums[i]);
+            recurse(nums, list, innerList, target, i, sum + nums[i]);
+            innerList.remove(innerList.size()-1);
+        }
+    }
+}
